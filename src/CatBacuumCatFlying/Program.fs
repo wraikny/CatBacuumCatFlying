8ec13a179c73aec839416f85d4ae40d3491319b3
wraikny <writtenknown@gmail.com>
2019-10-05 03:09:47 +0900

    setting

--- src/CatBacuumCatFlying/Program.fs
+++ src/CatBacuumCatFlying/Program.fs
@@ -5,7 +5,7 @@ open Affogato
 let setting = {
   areaSize = Vector2.init 1600 900
   playerSize = Vector2.init 128.0f 256.0f
-  flyingCatsSize = Vector2.init 64.0f 32.0f
+  flyingCatsSize = Vector2.init 128.0f 64.0f
 
   playerX = 200.0f
 
@@ -15,7 +15,7 @@ let setting = {
   initSpeeds = {
     bacuumSpeed = 8.0f
     fallingSpeed = 8.0f
-    flyingCatsSpeed = 4.0f
+    flyingCatsSpeed = 8.0f
   }
   diffSpeeds = {
     bacuumSpeed = 0.2f
@@ -23,14 +23,22 @@ let setting = {
     flyingCatsSpeed = 0.2f
   }
 
+  hp = 100.0f
+
+  generatePerMin = 30.0f, 2.0f
+  generateX = 3000.0f
+
   levelScoreStage = 100u
-  levelFrameStage = 60u
+  levelFrameStage = 60u * 10u
 }
 
 open Cbcf.View
 
 let viewSetting = {
   player = "animal_stand_neko.png"
+  score = "animal_stand_neko.png"
+  damage = "animal_stand_neko.png"
+  heal = "animal_stand_neko.png"
 }
 
 [<STAThread; EntryPoint>]

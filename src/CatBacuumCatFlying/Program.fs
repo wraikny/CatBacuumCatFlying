a677a1d46671f16b188e63fcd9411a7db5207552
wraikny <writtenknown@gmail.com>
2019-10-05 00:46:40 +0900

    paket

--- src/CatBacuumCatFlying/Program.fs
+++ src/CatBacuumCatFlying/Program.fs
@@ -1,5 +1,37 @@
 module Cbcf.Program
 open System
+open Affogato
+
+let setting = {
+  areaSize = Vector2.init 1600 900
+  playerSize = Vector2.init 128.0f 256.0f
+  flyingCatsSize = Vector2.init 64.0f 32.0f
+
+  playerX = 200.0f
+
+  ceilingHeight = 100.0f
+  floorHeight = 800.0f
+
+  initSpeeds = {
+    bacuumSpeed = 8.0f
+    fallingSpeed = 8.0f
+    flyingCatsSpeed = 4.0f
+  }
+  diffSpeeds = {
+    bacuumSpeed = 0.2f
+    fallingSpeed = 0.2f
+    flyingCatsSpeed = 0.2f
+  }
+
+  levelScoreStage = 100u
+  levelFrameStage = 60u
+}
+
+open Cbcf.View
+
+let viewSetting = {
+  player = "animal_stand_neko.png"
+}
 
 [<STAThread; EntryPoint>]
 let main _ =
